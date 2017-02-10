a2fa6afbe5ae18565ba20f852d89fc334d20d555
xmx2002 <xmx2002@gmail.com>
2017-02-10 18:09:24 +0800

    add the pic_url to postsql

--- ftest/ftest/spiders/freebuf.py
+++ ftest/ftest/spiders/freebuf.py
@@ -9,44 +9,77 @@ dir_path = '%s/%s' % (ftest.settings.IMAGES_STORE,"freebuf")
 
 def gen_link():
     url=['http://www.freebuf.com/articles']
-    for i in range(2,225):
-        url.append('http://www.freebuf.com/articles/page/'+str(i))
+    #for i in range(2,225):
+        #url.append('http://www.freebuf.com/articles/page/'+str(i))
     return url
 
-def inser_db(db_id,url):
+def inser_db_url(url):
     #sql_desc="INSERT INTO postgresql_1(fullname, username, organization, mail, joined,followers,starred,following,popular_repos,popular_repos_download,popular_repos_star,popular_repos_info,home_page)values(item['fullname'], item['username'], item['organization'], item['mail'],item['joined'],item['followers'],item['starred'],item['following'],item['popular_repos'],item['popular_repos_download'],item['popular_repos_star'],item['popular_repos_info'], item['home_page'])"
     conn = psycopg2.connect(database="freebuf", user="postgres", password="123456", host="127.0.0.1", port="5432")
     try:
         cur=conn.cursor()
         #ret=cur.execute("CREATE TABLE URL (ID INT PRIMARY KEY NOT NULL,URL TEXT NOT NULL);")
-        
-        print type(db_id)
         #cur.execute("INSERT INTO URL (ID,URL) VALUES (%d,%s)",(id,url));
-        cur.execute("INSERT INTO URL (ID,URL) VALUES (%s,%s)", (str(db_id),url,));
-        
-
+        #cur.execute("INSERT INTO URL (ID,URL) VALUES (%s,%s)", (str(db_id),url,));
+        cur.execute("INSERT INTO url (URL) VALUES (%s)", (url,));
         conn.commit()
-        log.msg("Data added to PostgreSQL database!",
-            level=log.DEBUG,spider=spider)
 
     except Exception,e:
-        print 'insert record into table failed'
+        print 'insert record into url failed'
         print e
-      
-
     finally:
         if cur:
             cur.close()
     conn.close()
     return
 
-def select_db(url):
+def select_db_url(url):
     #sql_desc="INSERT INTO postgresql_1(fullname, username, organization, mail, joined,followers,starred,following,popular_repos,popular_repos_download,popular_repos_star,popular_repos_info,home_page)values(item['fullname'], item['username'], item['organization'], item['mail'],item['joined'],item['followers'],item['starred'],item['following'],item['popular_repos'],item['popular_repos_download'],item['popular_repos_star'],item['popular_repos_info'], item['home_page'])"
     conn = psycopg2.connect(database="freebuf", user="postgres", password="123456", host="127.0.0.1", port="5432")
     try:
         cur=conn.cursor()
         #cur.execute("SELECT URL from URL")
-        cur.execute("SELECT URL from URL where URL= %s",(url,))
+        cur.execute("SELECT URL from url where URL= %s",(url,))
+        rows = cur.fetchall()
+        if rows:    
+            retval = True
+        else:
+            retval = False
+        return retval
+    except Exception,e:
+        print 'no record in url'
+        print e
+
+    finally:
+        if cur:
+            cur.close()
+    conn.close()
+    return False
+def inser_db_pic(pic_url,html_name,local_pic="not set",replaced=0):
+    #sql_desc="INSERT INTO postgresql_1(fullname, username, organization, mail, joined,followers,starred,following,popular_repos,popular_repos_download,popular_repos_star,popular_repos_info,home_page)values(item['fullname'], item['username'], item['organization'], item['mail'],item['joined'],item['followers'],item['starred'],item['following'],item['popular_repos'],item['popular_repos_download'],item['popular_repos_star'],item['popular_repos_info'], item['home_page'])"
+    conn = psycopg2.connect(database="freebuf_pic", user="postgres", password="123456", host="127.0.0.1", port="5432")
+    try:
+        cur=conn.cursor()
+        cur.execute("INSERT INTO pic_replace (PIC_URL,HTML_NAME,LOCAL_PIC,REPLACE) VALUES (%s,%s,%s,%s)", (pic_url,html_name,local_pic,str(replaced),));
+        conn.commit()
+
+    except Exception,e:
+        print 'insert record into pic failed'
+        print e
+      
+    finally:
+        if cur:
+            cur.close()
+    conn.close()
+    return
+
+def select_db_pic(url):
+    #sql_desc="INSERT INTO postgresql_1(fullname, username, organization, mail, joined,followers,starred,following,popular_repos,popular_repos_download,popular_repos_star,popular_repos_info,home_page)values(item['fullname'], item['username'], item['organization'], item['mail'],item['joined'],item['followers'],item['starred'],item['following'],item['popular_repos'],item['popular_repos_download'],item['popular_repos_star'],item['popular_repos_info'], item['home_page'])"
+    conn = psycopg2.connect(database="freebuf", user="postgres", password="123456", host="127.0.0.1", port="5432")
+    try:
+        cur=conn.cursor()
+        #cur.execute("SELECT URL from URL")
+        cur.execute("SELECT PIC_URL from PIC_URL where PIC_URL= %s",(url,))
         rows = cur.fetchall()
         if rows:
             retval = True
@@ -54,7 +87,7 @@ def select_db(url):
             retval = False
         return retval
     except Exception,e:
-        print 'no record in table'
+        print 'no record in pic'
         print e
 
     finally:
@@ -67,16 +100,14 @@ class FreebufSpider(scrapy.Spider):
     #allowed_domains = ["freebuf.com"]
     #start_urls = ['http://www.freebuf.com/articles']
     start_urls = gen_link()
-    db_id = 1
     def parse(self, response):
         
         for href in response.xpath('//dl/dt/a/@href'):
             url = response.urljoin(href.extract())
             
-            if not select_db(url):
+            if not select_db_url(url):
                 #print 'insert '+ url
-                inser_db(self.db_id,url)
-                self.db_id+=1
+                inser_db_url(url)
                 yield scrapy.Request(url, callback=self.parse_dir_contents)
 
     def parse_dir_contents(self, response):
@@ -99,7 +130,8 @@ class FreebufSpider(scrapy.Spider):
                 #print item['pic_url']
                 us = item['pic_url'].split('/')[3:]
                 image_file_name = '_'.join(us)
-                file_path = '%s/%s' % (dir_path, image_file_name)                
+                file_path = '%s/%s' % (dir_path, image_file_name)
+                inser_db_pic(sel,filename,local_pic=file_path,replaced=0)
                 #item['pic_url'] = sel.xpath('a/@href').extract()
                 #item['pic_local'] = file_path
                 yield item        
